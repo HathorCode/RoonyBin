8e97434f6b7abe427c364d4dea2ae68b65c2e4e9
Corey Wieser <worey1@gmail.com>
2017-02-21 18:37:55 -0500

    im an idiot

--- src/engine/io.hpp
+++ src/engine/io.hpp
@@ -9,16 +9,17 @@ namespace rb {
   struct IO{
     /*Balls to the wall no error checking, totally wont ever bite us in the ass*/
     char* loadFromFile(const char* fileName) {
-      char *fileContents;
-      long inputSize;
-      FILE *inputFile = fopen(fileName, "rb");
-      fseek(inputFile, 0, SEEK_END);
-      inputSize = ftell(inputFile);
-      rewind(inputFile);
-      fileContents = (char*)malloc(inputSize * (sizeof(char)));
-      fread(fileContents, sizeof(char), inputSize, inputFile);
-      fclose(inputFile);                                // close the stream
-      return fileContents;
+      FILE *f = fopen(fileName, "rb");
+  		fseek(f, 0, SEEK_END);
+  		long fsize = ftell(f);
+  		fseek(f, 0, SEEK_SET);
+
+  		char *string = (char *)malloc(fsize + 1);
+  		fread(string, fsize, 1, f);
+  		fclose(f);
+
+  		string[fsize] = 0;
+  		return string;
   }
   }io;
 }
